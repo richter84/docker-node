cee0d59902dd4a942b6765917d8f01b04fd7ac5b
Richie <nncrich1984@gmail.com>
2024-01-05 14:18:01 +0000

    updated debug

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ app.use(cors())
 app.use(express.json());
 
 app.get('/api/v1', (req, res) => {
-  res.send('Hello World!!');
+  res.send('Hello Docker image');
   console.log("yeah it ran");
 });
 
